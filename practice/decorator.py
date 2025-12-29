e603ed896eb490983910331d06fa8638e1f75758
krsna0406 <krsna.dokula@gmail.com>
2025-12-29 18:14:35 +0530

    commiting the files to check

---
+++ practice/decorator.py
@@ -0,0 +1,19 @@
+
+
+def decorfun(func):
+    def inner(name):
+        print("inner of decor")
+        if name=="KITTU":
+            print("#"*30)
+            print("HELLO----",name)
+            print("#"*30)
+        else:
+            func(name)
+    return inner
+
+@decorfun
+def wish(name):
+    print("hi ",name)
+
+wish("krishna")
+wish("KITTU")
